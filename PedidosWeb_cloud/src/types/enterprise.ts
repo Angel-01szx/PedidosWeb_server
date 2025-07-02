061876670b3dc52a8e4c07a5b7e4340a1ed5a38a
Angel-01szx <angel.bp.d@outlook.com>
2025-07-02 18:14:45 -0500

    Delete PedidosWeb_cloud directory

--- PedidosWeb_cloud/src/types/enterprise.ts
+++
@@ -1,6 +0,0 @@
-class enterprise {
-    static ruc: string = '20556936685';
-    static razon: string = 'DISTRIBUIDORA Y COMERCIAL LISAORE EIRL'
-  }
-  
-  export default enterprise;
--- PedidosWeb_cloud/src/types/ipconfig.ts
+++
@@ -1,6 +0,0 @@
-class ipconfig {
-    //static url: string = '192.168.68.59';deedperu
-    static url: string = '192.168.0.19:3001';
-  }
-  
-  export default ipconfig;
